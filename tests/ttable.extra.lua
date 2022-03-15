dc4a5adb0cad83a2e1a278bd0a59f30e52454226
Tomás Guisasola <tomas@puc-rio.br>
2022-03-14 21:32:56 -0300

    Test corrections on number serialization.

--- tests/ttable.extra.lua
+++ tests/ttable.extra.lua
@@ -9,9 +9,9 @@ assert (table.fullconcat{} == "")
 assert (table.fullconcat{a=1} == "a=1")
 assert (table.fullconcat{a=true} == "a=true")
 local s = '{'..table.fullconcat ({a=1,b='2'}, ':', ', ', tostring, quote)..'}'
-assert (s == [[{a:1, b:'2'}]] or s == [[{b:'2', a:1}]])
+assert (s == [[{a:((1)), b:'2'}]] or s == [[{b:'2', a:((1))}]])
 local s = table.fullconcat ({["a'b"]=1, b="q'a"}, " -> ", " AND ", quote, quote)
-assert (s == "'a''b' -> 1 AND 'b' -> 'q''a'", ">>"..s.."<<")
+assert (s == "'a''b' -> ((1)) AND 'b' -> 'q''a'", ">>"..s.."<<")
 local ok, err = pcall (table.pfullconcat, {a = {}})
 assert (not ok, err:match"string expected, got table")
 local ok, err = pcall (table.pfullconcat, 2)
@@ -27,7 +27,7 @@ local k,v = table.twostr{a=true}
 assert (k=="a" and v=="true")
 local k,v = table.twostr ({a=1,["q'w"]="a'b",}, "-", "=", quote, quote)
 assert ( (k=="'a'-'q''w'" or k=="'q''w'-'a'") and
-         (v=="1='a''b'" or v=="'a''b'=1") )
+         (v=="((1))='a''b'" or v=="'a''b'=((1))") )
 io.write"."
 
 print(' '..table._VERSION.." Ok!")
